c3679f6b0dab53221573b91e5c58f274bbb767e9
Filip Kulic <filip.kulic.dev@outlook.com>
2025-05-14 10:53:08 +0200

    Made App and Die components with styles

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import Die from "./Die";
+
+export default function App() {
+
+  return (
+    <main className="container">
+      <div className="dice-container">
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
+    </main>
+  )
+}
---
+++ src/components/Die.jsx
@@ -0,0 +1,10 @@
+export default function Die({ value }) {
+
+    return (
+
+        <button className="die-btn">
+            {value}
+        </button>
+    
+    )
+}
