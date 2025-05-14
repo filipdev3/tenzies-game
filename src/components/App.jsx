32f92cbd62ffd3b2bd95d6e5baa6274508aa03ef
Filip Kulic <filip.kulic.dev@outlook.com>
2025-05-14 14:47:25 +0200

    Turn dice from arr of numbers into arr of objects, import 'nanoid' to generate unique ids for every die

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./Die";
 
 export default function App() {
 
 
 // Hold generated array into state
-const [diceNumbers, setDiceNumbers] = useState(generateAllNewDice())
+const [dice, setDice] = useState(generateAllNewDice())
 
 
 // Generate array with 10 items with random value from 1 to 6
 function generateAllNewDice() {
-  return new Array(10)                              // generate new Array with 10 item spaces
-    .fill(0)                                        // fill array with items with value of 0
-    .map(() => Math.floor(Math.random() * 6) + 1)   // map over every item and transform 0 into a random number between 1 and 6
+  return new Array(10)
+    .fill(0)
+    .map(() => ({
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: false,
+        id: nanoid()
+    }))
 }
 
 
+
+// Roll dice function
 const rollDice = () => {
-  setDiceNumbers(generateAllNewDice)
+  setDice(generateAllNewDice)
 }
 
 
 // Map over state items to generate Die components 
-const diceElements = diceNumbers.map(die => {
-  return <Die value={die}/>
+const diceElements = dice.map(die => {
+  return <Die key={die.id} value={die.value}/>
 })
 
 
